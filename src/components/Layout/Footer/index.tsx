e8ea4346f722a837bdc9eb5c2964e9728388ff8f
shuijing.zhang <shuijing.zhang@tuya.com>
2022-04-12 00:19:46 +0800

    ci: 🎡 小楼昨夜又东风

--- src/components/Layout/Footer/index.tsx
+++ src/components/Layout/Footer/index.tsx
@@ -9,17 +9,17 @@ const Footer: React.FC<FooterProps> = (props) => {
     <div className={cs(styles.footer, className)}>
       <div className={styles.footerInfo} style={style}>
         <a href='https://github.com/zlqxk/awesome-blog'>Github</a>
-        <a href='https://beian.miit.gov.cn' target='_blank'>
+        <a href='https://beian.miit.gov.cn' target='_blank' rel='noreferrer'>
           浙ICP备2022010667号
         </a>
       </div>
       <div className={styles.footerPvUv} id='busuanzi_container_site_pv'>
-        <span>
+        <p>
           本站总访问量<span id='busuanzi_value_site_pv'></span>次
-        </span>
-        <span>
+        </p>
+        <p>
           访客数<span id='busuanzi_value_site_uv'></span>人次
-        </span>
+        </p>
       </div>
     </div>
   )
