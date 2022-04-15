32b79f7f6ccbd1c565355dc23202235e80a38194
shuijing.zhang <shuijing.zhang@tuya.com>
2022-04-15 17:04:28 +0800

    test: 💍 人面不知何处去,桃花依旧笑春风

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,6 +11,10 @@ class MyDocument extends Document {
             name='description'
             content='学习和分享工作生活中遇到的问题，记录前沿的技术，分享技术文章。'
           />
+          <meta
+            name='google-site-verification'
+            content='s-VsAQCdV_MrjX9LC-rvr6SZ-MYSM4y1vVMJ9Fw7Oxo'
+          />
         </Head>
         <script async src='//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js'></script>
         <body className='dark'>
